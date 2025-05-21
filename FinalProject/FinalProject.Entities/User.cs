988c249307a9cfd76e77047ea6b0acc1cb1e14d5
pranavalai <96616742+pranavalai@users.noreply.github.com>
2025-05-21 20:24:07 +0530

    a1

--- FinalProject/FinalProject.Entities/User.cs
+++ FinalProject/FinalProject.Entities/User.cs
@@ -8,7 +8,7 @@ namespace FinalProject.Entities
 {
     public class User
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
